8ad5051bc12c6aedcd6ddd1c857faab76d5b7a23
Dmit130489 <dmit.89@list.ru>
2024-05-19 15:59:27 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,11 @@
+tuple_ = 'I, D, D, Q, D'
+immutable_var = tuple_
+print(immutable_var)
+
+#Изменить данные кортежа не возможно т.к. фишка кортежа заключается в его неизменности данных.
+
+stmutable_li = [tuple_, 'doll', 'table']
+print(stmutable_li)
+
+
+
